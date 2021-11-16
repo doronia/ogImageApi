baada51e1bbf7ef29c031f9e70826e6764600dc5
doron.info@gmail.com <doron.info@gmail.com>
2021-11-16 11:34:43 +0200

    .

--- api/og.js
+++ api/og.js
@@ -2,25 +2,24 @@ const getHtml = require('../utils/getHtml');
 const getPage = require('../utils/getPage');
 
 module.exports = async (req, res) => {
-
-    // const {title = null, tags = null, author = 'חדשות ספונסר'} = req;
-
-    const _title = 'בנק מזרחי מתחיל להוות תחרות לפועלים ולאומי';
-    const _tag = `דוחות כספיים`;
+    const {title = null, tag = null, author = 'חדשות ספונסר'} = req.query;
+    const _title = title;
+    const _tag = tag;
+    const _author = author;
+    const viewport = {w: 1200, h: 628};
 
     try {
-        const html = await getHtml(_title, _tag);
+        const html = await getHtml(viewport.w, viewport.h, _title, _tag, _author);
         // res.setHeader('Content-Type', 'text/html; charset=UTF-8');
         // res.end(html);
 
         const page = await getPage();
-        await page.setViewport({width: 1200, height: 630});
+        await page.setViewport({width: viewport.w, height: viewport.h});
         await page.setExtraHTTPHeaders({
             'Accept-Charset': 'utf-8',
             'Content-Type': 'text/html; charset=utf-8',
         });
         await page.setContent(html.toString('utf8'), {waitUntil: 'networkidle2', timeout: 15000});
-
         const buffer = await page.screenshot({type: 'png'});
 
         res.setHeader('Cache-Control', `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`);
@@ -31,8 +30,6 @@ module.exports = async (req, res) => {
         res.setHeader('Content-Type', 'text/html');
         res.end('<h1>Internal Error</h1><p>Sorry, there was a problem</p>');
     }
-
-
 
 
 };
